b8e549516d3577e8f74fcf1cf3dfdcc330d1826f
Mariana Nogueira <mariana.nogueira@ibsolution.com.br>
2021-06-09 12:40:59 -0300

    Adicionando entidade employee em domain

---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public string Registration { get; set; }
+
+        public string Area { get; set; }
+
+        public string GrossWage { get; set; }
+
+        public string AdmissionDate { get; set; }
+    }
+}
